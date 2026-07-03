345d15d2bb9a90dfa4a65630cf85394d9b50575e
BrillianHaikal89 <brillianulbi@gmail.com>
2026-07-03 20:02:41 +0700

    [updated] perbaiki db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,6 +2,14 @@ import pkg from "pg";
 const { Pool } = pkg;
 import dotenv from "dotenv";
 
+console.log("===== ENV CHECK =====");
+console.log("DATABASE_URL:", process.env.DATABASE_URL ? "ADA" : "TIDAK ADA");
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("=====================");
+
 dotenv.config();
 
 // ===============================
